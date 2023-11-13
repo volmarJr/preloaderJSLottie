c9cc519520901e106bfa2407e634a14a4b7a565b
volmarJr <150699379+volmarJr@users.noreply.github.com>
2023-11-13 13:51:31 -0300

    Update preloader.js

--- preloader.js
+++ preloader.js
@@ -38,7 +38,7 @@ function hideOverlay() {
         renderer: 'svg', // ou 'canvas' se preferir
         loop: true,
         autoplay: true,
-        path: 'https://volmarjr.github.io/preloaderJSLottie/Animation%20-%201699894098141.json',
+        path: 'https://volmarjr.github.io/preloaderJSLottie/anima.json',
     });
 }
 
